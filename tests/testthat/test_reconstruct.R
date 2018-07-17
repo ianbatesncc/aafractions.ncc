8a74b6462c426e04fc734c7e74409fa2baa910f8
Ian Bates <ian.bates@nottscc.gov.uk>
2018-07-17 11:11:16 +0100

    [tests] Split off reconstruct all versions

--- tests/testthat/test_reconstruct.R
+++ tests/testthat/test_reconstruct.R
@@ -11,16 +11,32 @@ test_that("reconstruct works", {
 
     expect_is(t1, "data.frame")
 
+})
 
-    t2 <- purrr::cross(list(
-        x = c("aaf_2017_phe", "aaf_2014_ljucph", "aaf_2008_ljucph", "aaf_2007_ni39")
-        , y = c("morbidity", "mortality")
-    )) %>%
-        lapply(function(l){
-            cat("INFO: (x, y) =(", paste(l$x, l$y, sep = ", "), ")", "\n")
-            reconstruct(l$x, l$y)
-        })
+test_that("reconstruct all versions and types", {
 
-    expect_length(t2, 8)
+    these_versions <- eval(formals(reconstruct)[[1]])
+    # c("aaf_2017_phe", "aaf_2014_ljucph", "aaf_2008_ljucph", "aaf_2007_ni39")
+    these_analysistypes <- eval(formals(reconstruct)[[2]])
+    # c("morbidity", "mortality")
+
+    t2 <- merge(
+        data.frame(
+            this_version = these_versions
+            , stringsAsFactors = FALSE
+        )
+        , data.frame(
+            this_analysistype = these_analysistypes
+            , stringsAsFactors = FALSE
+        )
+    ) %>% mutate(tname = paste(this_version, this_analysistype, sep = "__")) %>%
+    {mapply(
+        function(v0, v1, v2){list(reconstruct(v1, v2, verbose = FALSE))}
+        , .$tname, .$this_version, .$this_analysistype
+    )}
+
+    expect_length(t2, length(these_versions) * length(these_analysistypes))
+
+
 
 })
