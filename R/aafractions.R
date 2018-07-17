e88e8ff22041893abd321ba652be81d220ac106f
Ian Bates <ian.bates@nottscc.gov.uk>
2018-07-17 12:01:46 +0100

    [project] Update some documentation

--- R/aafractions.R
+++ R/aafractions.R
@@ -1,14 +1,16 @@
-#
-# aafractions.ncc
-#
-
 #' aafractions.ncc: Alcohol Attributable Fractions Lookup Tables
 #'
 #' Provides lookup tables for use with alcohol attributable fractions analyses.
 #'
-#' @section Foo functions:
-#' The foo functions ...
+#' @section Functions:
+#'
+#' \code{\link{reconstruct}}
+#'
+#'
+# @import dplyr
+# @importFrom data.table setDT dcast.data.table
 #'
 #' @docType package
 #' @name aafractions.ncc
+#'
 NULL
