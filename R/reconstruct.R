5f09d9a2e89c63744821b52254fa4ff1e3f6ad73
Ian Bates <ian.bates@nottscc.gov.uk>
2019-02-27 09:11:28 +0000

    [project] reconstruct amendments.
     - whitespace
     - comments
     - tests
     - other documentation

--- R/reconstruct.R
+++ R/reconstruct.R
@@ -5,7 +5,8 @@ condition_uid = NULL
 
 #' Reconstruct lu tables
 #'
-#' Reconstruct alcohol attributable fractions table for given version and analysis type.
+#' Reconstruct alcohol attributable fractions table for given version and
+#' analysis type.
 #'
 #' @param this_version table version to reconstruct
 #' @param this_analysistype specify analysis type: either morbidity or mortality
@@ -15,10 +16,24 @@ condition_uid = NULL
 #' @import dplyr
 #' @importFrom data.table setDT dcast.data.table
 #'
+#' @examples
+#'
+#' t1 <- reconstruct("aaf_2017_phe", "morbidity")
+#' str(t1)
+#'
+#' t2 <- reconstruct("aaf_2017_phe", "morbidity", molten = TRUE)
+#' str(t2)
+#'
+#'
 #' @export
 #'
 reconstruct <- function(
-    this_version = c("aaf_2017_phe", "aaf_2014_ljucph", "aaf_2008_ljucph", "aaf_2007_ni39")
+    this_version = c(
+        "aaf_2017_phe"
+        , "aaf_2014_ljucph"
+        , "aaf_2008_ljucph"
+        , "aaf_2007_ni39"
+    )
     , this_analysistype = c("morbidity", "mortality")
     , verbose = TRUE
     , molten = FALSE
@@ -27,9 +42,11 @@ reconstruct <- function(
     this_analysistype <- match.arg(this_analysistype)
 
     if (verbose) {
-        cat("INFO: reconstruct: (version, analysis_type) = ("
+        cat(
+            "INFO: reconstruct: (version, analysis_type) = ("
             , paste(this_version, this_analysistype, sep = ", "), ")"
-            , "\n")
+            , "\n"
+        )
     }
 
     this_table <- aafractions.ncc::aa_versions %>%
@@ -37,16 +54,18 @@ reconstruct <- function(
         merge(aafractions.ncc::aa_conditions, by = "condition_uid") %>%
         mutate(analysis_type = this_analysistype) %>%
         merge(
-            aafractions.ncc::lu_fractions %>% select(-condition_uid)
-            , by = c("Version", "analysis_type", "condition_fuid")
+            aafractions.ncc::aa_fractions
+            , by = c("Version", "analysis_type", "condition_uid")
         )
 
     if (!molten) {
         this_table <- this_table %>%
             data.table::setDT() %>%
-            data.table::dcast.data.table(... ~ aa_ageband + sex, value.var = "aaf", fun = sum) %>%
+            data.table::dcast.data.table(
+                ... ~ aa_ageband + sex, value.var = "aaf", fun = sum
+            ) %>%
             as.data.frame()
     }
 
-    invisible(this_table)
+    this_table
 }
